8b374780fb05e7aa9787abe58609518d44cc5f2b
AleLoiac <alessandroloiac@gmail.com>
2023-04-06 12:29:27 +0200

    06/04

--- checkers/x/checkers/types/errors.go
+++ checkers/x/checkers/types/errors.go
@@ -7,9 +7,6 @@ import (
 )
 
 // x/checkers module sentinel errors
-var (
-	ErrSample = sdkerrors.Register(ModuleName, 1100, "sample error")
-)
 
 var (
 	ErrInvalidBlack     = sdkerrors.Register(ModuleName, 1100, "black address is invalid: %s")
