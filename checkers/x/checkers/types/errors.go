efac8c7aeaf4df8e668e5fef789250d33b67ca19
AleLoiac <alessandroloiac@gmail.com>
2023-04-21 21:01:16 +0200

    21/04

--- checkers/x/checkers/types/errors.go
+++ checkers/x/checkers/types/errors.go
@@ -11,4 +11,8 @@ var (
 	ErrInvalidBlack     = sdkerrors.Register(ModuleName, 1100, "black address is invalid: %s")
 	ErrInvalidRed       = sdkerrors.Register(ModuleName, 1101, "red address is invalid: %s")
 	ErrGameNotParseable = sdkerrors.Register(ModuleName, 1102, "game cannot be parsed")
+
+	ErrInvalidGameIndex     = sdkerrors.Register(ModuleName, 1103, "game index is invalid")
+	ErrInvalidPositionIndex = sdkerrors.Register(ModuleName, 1104, "position index is invalid")
+	ErrMoveAbsent           = sdkerrors.Register(ModuleName, 1105, "there is no move")
 )
